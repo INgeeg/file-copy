6fbbdcda226e7a09fbd62db941123d1e16c2b296
unknown <aabdu@webserver.cosmostx.org>
2014-11-25 11:20:28 -0600

    folder copy complete

--- Class1.cs
+++ Class1.cs
@@ -93,12 +93,30 @@ namespace cmd
                     }
                 }
                 if (!exists) {
-                    Console.WriteLine(sc.FullName);
-                    Console.WriteLine(trg.FullName);
-                    //DirectoryCopy(sc.FullName, trg.FullName,true);
+                    CopyFolder(sc.FullName, trg.FullName +"\\" +sc.Name);
                 }
             }
 
+        }
+
+        static void CopyFolder(string sourceFolder, string destFolder)
+        {
+            if (!Directory.Exists(destFolder))
+                Directory.CreateDirectory(destFolder);
+            string[] files = Directory.GetFiles(sourceFolder);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destFolder, name);
+                File.Copy(file, dest);
+            }
+            string[] folders = Directory.GetDirectories(sourceFolder);
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destFolder, name);
+                CopyFolder(folder, dest);
+            }
         }
 
         public static void Main(string[] args ) {
